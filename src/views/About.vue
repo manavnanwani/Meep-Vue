6fecdee318a3a6766abf456f2c169efbd625b7dd
Manav Nanwani <manav.nanwani@gmail.com>
2021-04-04 04:34:37 +0530

    done

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,8 @@
     <b-container>
     <b-row alogn-v="center" align-h="center" class="mt-md-5">
       <b-col cols="11" md="5" class="site-reg-main mt-md-5">
-        <h2 class="text-center">Square Bubbles</h2>
-        <p class="text-center mt-5">
-            Team Members:<br>
-            1.<br>
-            2.<br>
-            3.<br>
-            4.<br>
-            5.<br>
-        </p>
+        <h1 class="text-center display-5">Submitted By</h1>
+        <h1 class="text-center display-4">Square Bubbles</h1>
       </b-col>
     </b-row>
   </b-container>
